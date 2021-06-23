46c3c6f86aa804d5332c85a18e66592b353eba26
leooriginal <notefo660>
2021-06-23 20:56:53 +0300

    some changes

--- testWebServer.js
+++ testWebServer.js
@@ -1,6 +1,17 @@
-var http=require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200,{'Content-Type':'text/plain'});
-  response.end('Hello!');
-}).listen(8081);
+var http = require('http');
+var fs = require('fs');
+
+const PORT=8081;
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;
+
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(PORT);
+});
+
 console.log('..srv running');
