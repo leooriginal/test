930b1be3ffaaeca27a6d95527f8641481607f02f
leooriginal <leooriginal@mail.ru>
2021-07-17 18:40:10 +0300

    some changes

--- testConsole.js
+++ testConsole.js
@@ -1,7 +1,11 @@
-function test (a,b,c) {
-  var test;
-  test = a+b+c;
-  return test;
-//console.log (test + ' cm');
+function test (a, b, c) {
+  var test
+  test = a + b + c
+  return test
+// console.log (test + ' cm');
 }
-console.log (test (3,5,6));
+console.log(test(3, 5, 6))
+if (test(5, 5, 5) !== 14) {
+  console.log('OK!!!')
+}
+console.log('ok!')
